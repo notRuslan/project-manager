c9aacde26a62e314674f2751ebc587eb759a0d63
notRuslan <ruslangr@yahoo.com>
2022-06-05 13:17:02 +0300

    add slim/slim

--- manager/public/index.php
+++ manager/public/index.php
@@ -1,13 +1,31 @@
 <?php
 
-declare(strict_types=1);
+//declare(strict_types=1);
 
-echo '<html lang="en">
-<head>
-  <title>App</title>
-  <meta charset="UTF-8" />
-</head>
-<body>
-  <h1>Hello, World!</h1>
-</body>
-</html>';
+use Slim\Http\Request;
+use Slim\Http\Response;
+//use \Psr\Http\Message\ServerRequestInterface as Request;
+//use \Psr\Http\Message\ResponseInterface as Response;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+http_response_code(500);
+
+(function () {
+    $app = new \Slim\App([
+                             'settings' => [
+                                 'addContentLengthHeader' => false,
+                                 'displayErrorDetails' => (bool)getenv('APP_DEBUG'),
+                             ],
+                         ]);
+
+    $app->get('/', function (Request $request, Response $response) {
+        return $response->withJson([
+                                       'name' => 'Manager',
+                                       'param' => $request->getQueryParam('param'),
+                                   ]);
+    });
+
+    $app->run();
+})();
