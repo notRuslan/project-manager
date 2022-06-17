9e2fa2f22d08dc4e9a5f100070f9c932203f3df6
notRuslan <ruslangr@yahoo.com>
2022-06-17 13:28:57 +0300

    Add user fixture and Oauth libs

--- manager/config/bundles.php
+++ manager/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
 ];
