b561857390231a7537c69bcfe7bd442280df30a2
notRuslan <ruslangr@yahoo.com>
2022-06-30 18:32:56 +0300

    Moved profile controller

--- manager/src/Controller/Profile/ShowController.php
+++ manager/src/Controller/Profile/ShowController.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace App\Controller;
+namespace App\Controller\Profile;
 
 use App\ReadModel\User\UserFetcher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProfileController extends AbstractController
+class ShowController extends AbstractController
 {
     private $users;
 
