8846692a2aa573b0484c7027bc6bc90bb67e4da5
notRuslan <ruslangr@yahoo.com>
2022-07-29 00:10:15 +0300

    add task file model

---
+++ manager/src/Migrations/Version20220728210549.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220728210549 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP SEQUENCE work_projects_tasks_seq CASCADE');
+        $this->addSql('CREATE TABLE work_projects_task_files (id UUID NOT NULL, task_id INT NOT NULL, member_id UUID NOT NULL, date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, info_path VARCHAR(255) NOT NULL, info_name VARCHAR(255) NOT NULL, info_size INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_B8A3E1028DB60186 ON work_projects_task_files (task_id)');
+        $this->addSql('CREATE INDEX IDX_B8A3E1027597D3FE ON work_projects_task_files (member_id)');
+        $this->addSql('CREATE INDEX IDX_B8A3E102AA9E377A ON work_projects_task_files (date)');
+        $this->addSql('COMMENT ON COLUMN work_projects_task_files.id IS \'(DC2Type:work_projects_task_file_id)\'');
+        $this->addSql('COMMENT ON COLUMN work_projects_task_files.task_id IS \'(DC2Type:work_projects_task_id)\'');
+        $this->addSql('COMMENT ON COLUMN work_projects_task_files.member_id IS \'(DC2Type:work_members_member_id)\'');
+        $this->addSql('COMMENT ON COLUMN work_projects_task_files.date IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE work_projects_task_files ADD CONSTRAINT FK_B8A3E1028DB60186 FOREIGN KEY (task_id) REFERENCES work_projects_tasks (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE work_projects_task_files ADD CONSTRAINT FK_B8A3E1027597D3FE FOREIGN KEY (member_id) REFERENCES work_members_members (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE work_projects_tasks_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('DROP TABLE work_projects_task_files');
+    }
+}
